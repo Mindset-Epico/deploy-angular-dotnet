082429bb220a8e3d76d412ad2378eae783501324
Pedro Leite <mindset.epico@gmail.com>
2023-12-01 20:15:26 -0300

    Fix05?

--- src/BibliotecaCorporativa/frontend/BibCorp-app/src/assets/environments/environments.ts
+++ src/BibliotecaCorporativa/frontend/BibCorp-app/src/assets/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiURL: 'https://bibliocorp.azurewebsites.net//api/',
-  fotoURL: 'https://bibliocorp.azurewebsites.net//resources/fotos/'
+  apiURL: 'https://bibliocorp.azurewebsites.net/api/',
+  fotoURL: 'https://bibliocorp.azurewebsites.net/resources/fotos/'
 }
